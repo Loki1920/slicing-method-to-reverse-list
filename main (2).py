e036fcaf372968cb533be81f965df7a71474983f
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-04 15:50:47 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,11 @@
+'''list slicing program to reverse an aaray'''
+
+def reverseList(A):
+  print(A[::-1])
+    
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+
+print("Reversed list is")
+reverseList(A)
